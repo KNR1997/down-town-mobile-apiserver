feb69c5a149d81e57459b32f3f4a4f0e94e475c1
KNR1997 <kethakaranasinghe@gmail.com>
2026-04-24 00:28:34 +0530

    feat: Users crud

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,1 +1,27 @@
-export class User {}
+import { CoreEntity } from 'src/common/entities/core.entity';
+import { Shop } from 'src/shops/entities/shop.entity';
+import { Column, Entity, Index, OneToMany } from 'typeorm';
+
+@Entity()
+export class User extends CoreEntity {
+  @Column()
+  name: string;
+
+  @Index({ unique: true })
+  @Column()
+  email: string;
+
+  @Column()
+  password: string;
+
+  @Column({ default: true })
+  is_active?: boolean;
+
+  @OneToMany(() => Shop, (shop) => shop.owner)
+  shops?: Shop[];
+
+  constructor(user?: Partial<User>) {
+    super();
+    Object.assign(this, user);
+  }
+}
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,11 +1,31 @@
 import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { User } from './entities/user.entity';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class UsersService {
-  create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+  constructor(
+    @InjectRepository(User)
+    private readonly userRepository: Repository<User>,
+  ) {}
+
+  async findUserByEmail(email: string): Promise<User | null> {
+    return await this.userRepository.findOne({
+      where: { email },
+    });
+  }
+
+  async create(createUserDto: CreateUserDto): Promise<User> {
+    const user = new User();
+
+    user.name = createUserDto.name;
+    user.email = createUserDto.email;
+    user.password = createUserDto.password;
+
+    return await this.userRepository.save(user);
   }
 
   findAll() {
