6d92cc0b48111fb68ffcf841409a8a4df6f29014
KNR1997 <kethakaranasinghe@gmail.com>
2026-05-15 19:08:09 +0530

    feat: class-vaildator annotations

--- src/categories/dto/update-category.dto.ts
+++ src/categories/dto/update-category.dto.ts
@@ -1,28 +1,35 @@
 import { ApiProperty } from '@nestjs/swagger';
+import { IsOptional, IsString } from 'class-validator';
 
 export class UpdateCategoryDto {
   @ApiProperty({
     description: 'The name of the product',
     example: 'Apple',
   })
+  @IsString()
   name: string;
 
   @ApiProperty({
     description: 'The slug of the product',
     example: 'apple',
   })
+  @IsString()
+  @IsOptional()
   slug: string;
 
   @ApiProperty({
     description: 'The icon of the product',
     example: 'fruit-icon',
   })
+  @IsString()
   icon: string;
 
   @ApiProperty({
     description: 'The details of the product',
     example: 'Some details',
   })
+  @IsString()
+  @IsOptional()
   details: string;
 
   @ApiProperty({
